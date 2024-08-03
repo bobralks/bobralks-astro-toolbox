27223a5e3c865632c7dba9c14d2c10abb4345063
mixxmac <mixxmac@gmail.com>
2024-08-03 15:59:26 -0400

    stackbit config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,42 @@
 import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cloudinary';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          // Content collections
+          {
+            name: 'blog',
+            label: 'Blog Posts',
+            folder: 'content/blog',
+            create: true,
+            fields: [
+              {
+                label: 'Title',
+                name: 'title',
+                widget: 'string',
+              },
+              {
+                label: 'Publish Date',
+                name: 'date',
+                widget: 'datetime',
+              },
+              {
+                label: 'Body',
+                name: 'body',
+                widget: 'markdown',
+              },
+            ],
+          }
+        ],
+      },
+    }),
+  ],
+});
